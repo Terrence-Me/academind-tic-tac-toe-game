a65942ac82194963a66695e3c68d3c6376ebd2a8
Terrence Meikle <terrence_meikle@yahoo.com>
2022-05-29 12:10:01 -0400

    added form validation

--- scripts/config.js
+++ scripts/config.js
@@ -6,4 +6,20 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = 'none';
   backdropElement.style.display = 'none';
+  formElement.firstElementChild.classList.remove('error');
+  errorsOutputElement.textContent = '';
+}
+
+function savePlayerConfig(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const enteredPlayername = formData.get('playername').trim();
+  if (!enteredPlayername) {
+    e.target.firstElementChild.classList.add('error');
+    errorsOutputElement.textContent = 'Please enter a valid name!';
+    return;
+  } else {
+    e.target.firstElementChild.classList.remove('error');
+  }
+  closePlayerConfig();
 }
