6b1385e23c5df2d88714190ee8f9fd618bc04fa7
Terrence Meikle <terrence_meikle@yahoo.com>
2022-05-29 18:12:47 -0400

    added logic for managing the game

--- scripts/game.js
+++ scripts/game.js
@@ -3,5 +3,25 @@ function startNewGame() {
     alert('please set custom player names for both players');
     return;
   }
+  activePlayerNameElement.textContent = players[activePlayer].name;
   gameAreaElement.style.display = 'block';
+}
+
+function swtichPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(e) {
+  if (e.target.tagName !== 'LI') {
+    return;
+  }
+
+  e.target.textContent = players[activePlayer].symbol;
+  e.target.classList.add('disabled');
+  swtichPlayer();
 }
